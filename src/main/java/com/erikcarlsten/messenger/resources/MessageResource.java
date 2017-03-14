e5d0d516ca47b866ce97b9513646c48f672e0ac8
Erik S Carlsten <ecarlste@gmail.com>
2017-03-14 13:29:43 -0500

    Added JaxB XmlRootElement conversion to the Message class
    - This will only be used until we set up JSON support as JSON is the preferred method for sending REST data
    - converted the MessageResource method getMessages() to return the messages stored in the MessageService instance

--- src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
+++ src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
@@ -1,17 +1,23 @@
 package com.erikcarlsten.messenger.resources;
 
+import com.erikcarlsten.messenger.model.Message;
+import com.erikcarlsten.messenger.service.MessageService;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/messages")
 public class MessageResource {
 
+    private final MessageService messageService = new MessageService();
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMessages() {
-       return "Hello World!";
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Message> getMessages() {
+       return messageService.getAllMessages();
     }
 
 }
