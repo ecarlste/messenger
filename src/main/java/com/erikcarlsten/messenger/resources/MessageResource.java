6248400fca4e9243633ea53cdc58e92bbe552da9
Erik S Carlsten <ecarlste@gmail.com>
2017-03-14 15:33:06 -0500

    Created method in the MessageResource to get a single message by id

--- src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
+++ src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.erikcarlsten.messenger.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -18,6 +19,13 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessages() {
        return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long messageId) {
+        return messageService.getMessage(messageId);
     }
 
 }
