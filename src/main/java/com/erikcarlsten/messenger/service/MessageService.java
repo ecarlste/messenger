5cf2188ad0804bcebc5b150d5f5e69a25c6573ff
Erik S Carlsten <ecarlste@gmail.com>
2017-03-14 14:19:38 -0500

    Updated MessageService to use the database stubs for getting messages

--- src/main/java/com/erikcarlsten/messenger/service/MessageService.java
+++ src/main/java/com/erikcarlsten/messenger/service/MessageService.java
@@ -1,21 +1,18 @@
 package com.erikcarlsten.messenger.service;
 
+import com.erikcarlsten.messenger.database.DatabaseClass;
 import com.erikcarlsten.messenger.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
 
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
+
     public List<Message> getAllMessages() {
-        Message m1 = new Message(1L, "Hello World!", "Erik Carlsten");
-        Message m2 = new Message(2L, "Hello Jersey!", "Erik Carlsten");
-
-        List<Message> messages = new ArrayList<>();
-        messages.add(m1);
-        messages.add(m2);
-
-        return messages;
+        return new ArrayList<>(messages.values());
     }
 
 }
