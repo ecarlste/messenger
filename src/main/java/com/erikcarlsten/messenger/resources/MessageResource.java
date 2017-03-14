d30d737750e47c36b4d90dd5bf28501dd23987b8
Erik S Carlsten <ecarlste@gmail.com>
2017-03-14 16:01:12 -0500

    Added MessageResource method to add a message

--- src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
+++ src/main/java/com/erikcarlsten/messenger/resources/MessageResource.java
@@ -3,10 +3,7 @@ package com.erikcarlsten.messenger.resources;
 import com.erikcarlsten.messenger.model.Message;
 import com.erikcarlsten.messenger.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -19,6 +16,13 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
        return messageService.getAllMessages();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
     }
 
     @GET
