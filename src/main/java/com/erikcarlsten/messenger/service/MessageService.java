891e17f01b784d9d9da2e90ec32eca9c2139f810
Erik S Carlsten <ecarlste@gmail.com>
2017-03-14 14:23:39 -0500

    Added additional methods to the MessageService class

--- src/main/java/com/erikcarlsten/messenger/service/MessageService.java
+++ src/main/java/com/erikcarlsten/messenger/service/MessageService.java
@@ -15,4 +15,26 @@ public class MessageService {
         return new ArrayList<>(messages.values());
     }
 
+    public Message getMessage(long id) {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message) {
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message) {
+        if (message.getId() <= 0) {
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id) {
+        return messages.remove(id);
+    }
+
 }
